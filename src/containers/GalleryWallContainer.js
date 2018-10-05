442664789ecc9b79b7253632a663f11827a954ea
ritz-w <ritz.wu@outlook.com>
2018-10-05 10:06:11 +0100

    multi wall working, deleted title components

--- src/containers/GalleryWallContainer.js
+++ src/containers/GalleryWallContainer.js
@@ -5,32 +5,40 @@ import GalleryWall from './GalleryWall';
 
 
 export default class GalleryWallContainer extends Component {
-    // state = {
-    //     activeItem: 1
-    // }
+    state = {
+        activeItem: 1
+    }
 
   renderWall = () => {
-    // return (this.props.currentWall === 1) ? 
-        return <GalleryWall wallId={0}
+    return (this.props.currentWall === 1) ? 
+        <GalleryWall wallId={0}
         className="full-screenable-node" 
         currentUserId={this.props.currentUserId}
         galleryInfo={this.props.galleries[0]}
         deleteArtwork={this.props.deleteArtwork} 
         setGalleriesState={this.props.setGalleriesState}
-        // captions={this.props.captions.wall1}
         editCaption={this.props.editCaption} 
         deleteCaption={this.props.deleteCaption} 
-        // // titles={this.props.titles.wall1} 
-        // editTitle={this.props.editTitle} 
-    //     // deleteTitle={this.props.deleteTitle} 
         />
-    //  : (this.props.currentWall === 2) ?
-    //     <GalleryWall wallId={1} 
-    //     className="full-screenable-node" 
-    //     />
-    // :  <GalleryWall wallId={2}
-        //     className="full-screenable-node" 
-        // />
+     : (this.props.currentWall === 2) ?
+        <GalleryWall wallId={1} 
+        className="full-screenable-node" 
+        currentUserId={this.props.currentUserId}
+        galleryInfo={this.props.galleries[1]}
+        deleteArtwork={this.props.deleteArtwork} 
+        setGalleriesState={this.props.setGalleriesState}
+        editCaption={this.props.editCaption} 
+        deleteCaption={this.props.deleteCaption} 
+        />
+    :  <GalleryWall wallId={2}
+        className="full-screenable-node"
+        currentUserId={this.props.currentUserId}
+        galleryInfo={this.props.galleries[2]}
+        deleteArtwork={this.props.deleteArtwork} 
+        setGalleriesState={this.props.setGalleriesState}
+        editCaption={this.props.editCaption} 
+        deleteCaption={this.props.deleteCaption} 
+        />
   }
 
   handleClick = (e, {name}) => {
@@ -40,11 +48,11 @@ export default class GalleryWallContainer extends Component {
   render() {
     return (
         <div>
-        {/* //     <Menu attached='top' tabular>
-        //         <Menu.Item name={"1"} active={this.state.activeItem === 1} onClick={this.handleClick} />
-        //         <Menu.Item name={"2"} active={this.state.activeItem === 2} onClick={this.handleClick} />
-        //         <Menu.Item name={"3"} active={this.state.activeItem === 3} onClick={this.handleClick} />
-        //     </Menu> */}
+             <Menu attached='top' tabular>
+                 <Menu.Item name={"1"} active={this.state.activeItem === 1} onClick={this.handleClick} />
+                 <Menu.Item name={"2"} active={this.state.activeItem === 2} onClick={this.handleClick} />
+                 <Menu.Item name={"3"} active={this.state.activeItem === 3} onClick={this.handleClick} />
+            </Menu>
             { this.renderWall() }
             
 
